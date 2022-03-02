28e6e8139af8fa72b26ed158502b7639c096f97d
Simon Parten <quafadas@gmail.com>
2022-03-02 21:10:29 +0100

    scalfmt works now...

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -14,93 +14,102 @@
  * limitations under the License.
  */
 
-import hurdat._
+import hurdat.*
 import viz.PlotTarget
 
-@main def readData() = 
-    val raw = scala.io.Source.fromFile("hurdat.json").mkString("")
-    val hurdat = upickle.default.read[Seq[HurdatSystem]](raw)
+@main def readData() =
+  val raw = scala.io.Source.fromFile("hurdat.json").mkString("")
+  val hurdat = upickle.default.read[Seq[HurdatSystem]](raw)
 
-    println(hurdat.last)
-
+  println(hurdat.last)
 
 @main def acquireData() =
-    println("Running data import")
+  println("Running data import")
+  // https://www.aoml.noaa.gov/hrd/hurdat/hurdat2-format.pdf
+  val rawTcData = requests.get("https://www.aoml.noaa.gov/hrd/hurdat/hurdat2.html")
 
-    // https://www.aoml.noaa.gov/hrd/hurdat/hurdat2-format.pdf
-    val rawTcData = requests.get("https://www.aoml.noaa.gov/hrd/hurdat/hurdat2.html")
+  println("got raw data")
+  // val html = Source.fromURL("https://www.aoml.noaa.gov/hrd/hurdat/hurdat2.html")
+  val s = rawTcData.text()
+  val lines = s.split("\r\n").toSeq
+  val length = lines.length
 
-    println("got raw data")
-    // val html = Source.fromURL("https://www.aoml.noaa.gov/hrd/hurdat/hurdat2.html")
-    val s = rawTcData.text()
-    val lines = s.split("\r\n").toSeq
-    val length = lines.length
+  // //  Remove the html tags and other guff
+  val cleanedLines = lines.drop(3).dropRight(5)
 
-    // //  Remove the html tags and other guff
-    val cleanedLines = lines.drop(3).dropRight(5)
-
-    val firstTwoAreLetters = "^[A-Za-z]{2}".r
-    val headerLines = cleanedLines.map { x =>
+  val firstTwoAreLetters = "^[A-Za-z]{2}".r
+  val headerLines = cleanedLines.map { x =>
     //     println(firstTwoAreLetters.findFirstIn(x))
-        firstTwoAreLetters.findFirstIn(x) match {
-        case Some(x) => 1
-        case _ => 0
-        }  
-    }
-    val hurricaneSections = headerLines.scanLeft(0)(_+_).drop(1)
-    val allData = (hurricaneSections zip headerLines zip cleanedLines) map { 
-    case ((a,b), c) => (a,b,c)
-    }
+    firstTwoAreLetters.findFirstIn(x) match
+      case Some(x) => 1
+      case _       => 0
+  }
 
-    def entryToTrack(e: String) = {
-    //println(e)
+  val hurricaneSections = headerLines.scanLeft(0)(_ + _).drop(1)
+  val allData = (hurricaneSections zip headerLines zip cleanedLines) map { case ((a, b), c) =>
+    (a, b, c)
+  }
 
-    val record = if e(16).toString().isBlank then RecordIdentifier.B else RecordIdentifier.valueOf(e(16).toString() )
-    val year = e.substring(0,4).toInt
-    val month = e.substring(4,6).toInt
-    val day = e.substring(6,8).toInt
+  def entryToTrack(e: String) =
+    val record = if e(16).toString().isBlank then RecordIdentifier.B else RecordIdentifier.valueOf(e(16).toString())
+    val year = e.substring(0, 4).toInt
+    val month = e.substring(4, 6).toInt
+    val day = e.substring(6, 8).toInt
     val hour = e.substring(10, 12).toInt
     val minute = e.substring(12, 14).toInt
     val recordType: RecordIdentifier = record
-    val status: SystemStatus = SystemStatus.valueOf(e.substring(19,21))  
+    val status: SystemStatus = SystemStatus.valueOf(e.substring(19, 21))
     val latitude: Double = e.substring(23, 27).toDouble
     val longditude: Double = e.substring(30, 35).toDouble
     val latHem: String = e(27).toString()
     val longHem: String = e(35).toString()
     val maxSustainedWind = e.substring(38, 41).toDouble
     val cp = e.substring(43, 47).toDouble
-    TrackInfo(year = year, month = month, day = day, hour=hour, minute = minute, 
-    recordType = recordType, systemStatus = status, longditude=longditude, latitude=latitude, latHemisphere = latHem, longHemisphere = longHem,
-    maxSustainedWind = maxSustainedWind, minPressure = cp )
-    }
+    TrackInfo(
+      year = year,
+      month = month,
+      day = day,
+      hour = hour,
+      minute = minute,
+      recordType = recordType,
+      systemStatus = status,
+      longditude = longditude,
+      latitude = latitude,
+      latHemisphere = latHem,
+      longHemisphere = longHem,
+      maxSustainedWind = maxSustainedWind,
+      minPressure = cp
+    )
 
-    def toTC(rawHurdat1: String) : HurdatSystem = {
+  def toTC(rawHurdat1: String): HurdatSystem =
     val rawHurdat = rawHurdat1.split(",")
     val basin = rawHurdat(0).take(2)
     val numberTemp = rawHurdat(0).take(4).takeRight(2).toInt
     val year = rawHurdat(0).takeRight(4).toInt
     val name = rawHurdat(1).replaceAll("\\s", "")
     //val trackEntries = rawHurdat(2).replaceAll("\\s", "").toInt
-    HurdatSystem(year=year, name=name, basin=basin, number=numberTemp)
-    }
-    val allTC = allData.filter(_._2 == 1).map{case(section, header, data) => (section, toTC(data))}
-    val tracks = allData.groupBy(_._1).view.mapValues(list => {list.drop(1).map{case(a,b,track) => entryToTrack(track)}}).view
-    val finalDbTes = allTC.head
+    HurdatSystem(year = year, name = name, basin = basin, number = numberTemp)
 
-    val extract = tracks.view(1)
+  val allTC = allData.filter(_._2 == 1).map { case (section, header, data) =>
+    (section, toTC(data))
+  }
 
-    finalDbTes._2.copy(track = extract)
+  val tracks1 = allData.groupBy(_._1).view
+  val tracks = tracks1.mapValues { list =>
+    list.drop(1).map { case (a, b, track) => entryToTrack(track) }
+  }.view
 
-    val finalTCDb: Seq[HurdatSystem] = allTC.map{
-        case(tempId, hurricane) => {
-        val hurTrack = tracks(tempId)
+  val finalDbTes = allTC.head
+  val extract = tracks.view(1)
 
-        hurricane.copy(track = hurTrack)
-    }}
-    
-    println("saving to JSON file")
-    os.write.over( os.pwd/ "hurdat.json",  upickle.default.write(finalTCDb, 2) )
-    os.write.over( os.pwd/ "docs" / "assets" / "hurdat.json",  upickle.default.write(finalTCDb, 2) )
+  finalDbTes._2.copy(track = extract)
 
-    
-    
+  val finalTCDb: Seq[HurdatSystem] = allTC.map { case (tempId, hurricane) =>
+    val hurTrack = tracks(tempId)
+
+    hurricane.copy(track = hurTrack)
+  }
+
+  //println("saving to JSON file")
+  os.write.over(os.pwd / "hurdat.json", upickle.default.write(finalTCDb, 2))
+  os.write.over(os.pwd / "docs" / "assets" / "huvrdat.json", upickle.default.write(finalTCDb, 2))
